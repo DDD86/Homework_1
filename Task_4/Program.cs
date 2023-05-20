930b39af24740c607513137bfa1cd736e385e4e9
Reezo1986 <Reezo1986@gmail.com>
2023-05-21 01:48:05 +0600

    Добавлена программа №3

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (number < 2){
+            Console.WriteLine("Введите число больше 1!");
+         }
+else{while (i <= number){    // Цикл работает до number включительно
+   if (i%2 == 0){
+         Console.WriteLine(i);
+    }
+      
+          i++;
+}}
+
