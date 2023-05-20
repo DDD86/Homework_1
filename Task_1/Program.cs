61bb2968ed79c02b53e163b2a0ada55caa614360
Reezo1986 <Reezo1986@gmail.com>
2023-05-21 00:55:40 +0600

    Добавили первую программу

---
+++ Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+if (first_number > second_number){
+    Console.WriteLine($"{first_number} больше, чем {second_number}");
+}
+else{
+    Console.WriteLine($"{second_number} больше, чем {first_number}");
+}
