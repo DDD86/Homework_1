b22f5bb6d51726e81c3fb9e5b32a206045850a6f
Reezo1986 <Reezo1986@gmail.com>
2023-05-21 01:07:05 +0600

    Добавили программу №2

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int third_number = Convert.ToInt32(Console.ReadLine());
+if (first_number > second_number && first_number > third_number){
+    Console.WriteLine($"{first_number} больше, чем {second_number} и {third_number}");
+}
+else if(second_number > first_number && second_number > third_number){
+    Console.WriteLine($"{second_number} больше, чем {first_number} и {third_number}");
+}
+else{
+    Console.WriteLine($"{third_number} больше, чем {first_number} и {second_number}"); 
+}
