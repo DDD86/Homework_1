934e0884d04ae2f1a3bbead6e538f230063af691
Reezo1986 <Reezo1986@gmail.com>
2023-05-25 10:31:33 +0600

    Задача №13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <= 999)
+{
+    Console.WriteLine(number%10);
+}
+else if (number > 999)
+{
+    string n = Convert.ToString(number);
+    Console.WriteLine(n[2]);
+}
+else
+{
+    Console.WriteLine("Введены некоректные данные! Введите трехзначное или более число!");
+}
