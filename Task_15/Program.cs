13a1ee6621efe3873d437c70cf2a370dae7c44e6
Reezo1986 <Reezo1986@gmail.com>
2023-05-25 10:40:29 +0600

    Задача №15

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Ура! Сегодня ВыХоДнОй!");
+}
+else if (number >= 1 && number <=5)
+{
+    Console.WriteLine("Рабочий день!");
+}
+else
+{
+Console.WriteLine("Введите число от 1 до 7!");
+}
+
