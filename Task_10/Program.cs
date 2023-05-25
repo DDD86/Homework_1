664b3f413833454f23bee5beed9c6ad256375f24
Reezo1986 <Reezo1986@gmail.com>
2023-05-25 19:56:51 +0600

    Добавлен цикл while в задачу №10(Task_10)

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,10 +1,15 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿int i = 1;
+while (i > 0){
+    Console.WriteLine("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
 if (number > 99 && number < 1000)
 {
     Console.WriteLine((number/10)%10);
+    break;
 }
 else
 {
-    Console.WriteLine("Введены некоректные данные!");
+    Console.WriteLine("Введены некоректные данные!");   
+}
+i++;
 }
